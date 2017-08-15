7e5ca3148569ce85785cbeb63325f1a2c7547f1f
Ilya Baldin <ibaldin@renci.org>
2017-08-15 15:41:35 -0400

    Fixing manpages and enabling ntp

--- vagrant/xdp-fedora25/setup-packages-post-reboot.sh
+++ vagrant/xdp-fedora25/setup-packages-post-reboot.sh
@@ -7,6 +7,6 @@ fi
 
 set -eux
 
-dnf install -y git gcc ncurses-devel elfutils-libelf-devel bc openssl-devel libcap-devel clang llvm bison flex glibc-devel.i686 ntp
+dnf install -y git gcc ncurses-devel elfutils-libelf-devel bc openssl-devel libcap-devel clang llvm bison flex glibc-devel.i686 ntp man-pages
 dnf install -y bcc-tools kernel-devel-$(uname -r) kernel-headers-$(uname -r) libbcc-examples python-pyroute2
-systemctl start ntpd
+systemctl enable --now ntpd
