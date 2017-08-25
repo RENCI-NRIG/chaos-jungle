8c07b01d06b3880b819b72066a35857cc5eedd6a
Ilya Baldin <ibaldin@renci.org>
2017-08-25 20:40:07 +0000

    Working version of basic TCP modification

---
+++ bpf/bcc/xdp_tcp_modify.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+# modify TCP payloads to keep checksum
+# probability distribution ? 
+# using either XDP or TC BPF hooks
+
+from bcc import BPF
+import pyroute2
+import time
+import sys
+import argparse
+
+flags = 0
+parser = argparse.ArgumentParser()
+
+parser.add_argument("interface", help="interface to bind program to")
+parser.add_argument("-s", "--src", help="TCP flow source IP address")
+parser.add_argument("-d", "--dest", help="TCP flow destination IP address")
+parser.add_argument("--sport", help="TCP flow source port")
+parser.add_argument("--dport", help="TCP flow destination port")
+parser.add_argument("-t", "--tc", action="store_true", help="Use TC ingress hook instead of XDP")
+
+args = parser.parse_args()
+
+device = args.interface
+
+if args.tc:
+    flags |= 2 << 0
+    mode = BPF.SCHED_CLS
+    ctxtype = "__sk_buff"
+else:
+    mode = BPF.XDP
+    ctxtype = "xdp_md"
+
+print("Binding to {} using method {}".format(device, mode))
+    
+# load from file
+
+b = BPF(src_file = "tcp_modify.c", cflags=["-w", "-DCTXTYPE=%s" % ctxtype], debug = 0)
+
+fn = b.load_func("xdp_tcp_mod_prog", mode)
+
+if mode == BPF.XDP:
+    b.attach_xdp(device, fn, flags)
+else:
+    ip = pyroute2.IPRoute()
+    ipdb = pyroute2.IPDB(nl=ip)
+    idx = ipdb.interfaces[device].index
+    ip.tc("add", "clsact", idx)
+    ip.tc("add-filter", "bpf", idx, ":1", fd=fn.fd, name=fn.name,
+          parent="ffff:fff2", classid=1, direct_action=True)
+
+flowcnt = b["flowcnt"]
+#prev = [0] * 256
+#print("Printing SYN packets for ports 22, 80 and other, hit CTRL+C to stop")
+while 1:
+    try:
+        #b.trace_print()
+        for k in flowcnt.keys():
+            val = flowcnt.get(k).value
+            i = k.value
+            print("{}: {} syns".format(i, val))
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print("Removing filter from device")
+        break;
+
+if mode == BPF.XDP:
+    b.remove_xdp(device)
+else:
+    ip.tc("del", "clsact", idx)
+    ipdb.release()
