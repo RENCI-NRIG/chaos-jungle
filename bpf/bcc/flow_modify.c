1e6be42e6ef16fb304d99f2b843e159a146a204b
Ilya Baldin <ibaldin@renci.org>
2017-08-29 20:51:59 +0000

    Working multi-flow matching tested for TCP

---
+++ bpf/bcc/flow_modify.c
@@ -0,0 +1,298 @@
+#define KBUILD_MODNAME "foo"
+#include <uapi/linux/bpf.h>
+#include <linux/in.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <linux/if_vlan.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
+#include <bcc/proto.h>
+
+// modify packet's TCP payload by swapping two 16-bit aligned
+// values to preserve TCP checksum. 
+
+struct flowrec {
+  uint32_t saddr, daddr;
+  uint16_t sport, dport;
+  int udp, active;
+};
+
+// if no flowspecs are given, flownum will be 0 and we can't have that
+BPF_ARRAY(flowspecs, struct flowrec, (FLOWNUM > 0 ? FLOWNUM : 1));
+
+// get IP protocol
+static inline struct iphdr* parse_ipv4(void *data, u64 nh_off, void *data_end) {
+    struct iphdr *iph = data + nh_off;
+
+    if ((void*)&iph[1] > data_end)
+        return 0;
+    return iph;
+}
+
+// swap u16-s at offset 1 and 2, but do not go outside the payload end
+static inline int swapu16(uint16_t *start, void *data_end, int off1, int off2) {
+  uint16_t poff1, poff2;
+
+  // check first octet
+  if (((void*)&start[off1] > data_end) || ((void*)&start[off2] > data_end)) 
+    return 0;
+
+  // check second octet
+  if (((void*)&start[off1+1] > data_end) || ((void*)&start[off2 + 1] > data_end))
+    return 0;
+  
+  poff1 = start[off1];
+  poff2 = start[off2];
+
+  start[off2] = poff1;
+  start[off1] = poff2;
+
+  return 1;
+}
+
+// test packet headers against TCP flow record
+static inline int testTcpFlow(struct iphdr *iph, struct tcphdr *tcph, struct flowrec *flow) {
+  if (flow != NULL) {
+    // make sure it is an active TCP flow record
+    if ((flow->active == 0) || (flow->udp != 0))
+      return 0;
+
+    if ((flow->saddr != 0) && (iph->saddr != flow->saddr))
+      return 0;
+
+    if ((flow->daddr != 0) && (iph->daddr != flow->daddr))
+      return 0;
+
+    if ((flow->sport != 0) && (tcph->source != flow->sport))
+      return 0;
+
+    if ((flow->dport != 0) && (tcph->dest != flow->dport))
+      return 0;
+
+    return 1;
+  }
+  return 0;
+}
+
+// test packet headers against UDP flow record
+static inline int testUdpFlow(struct iphdr *iph, struct udphdr *udph, struct flowrec *flow) {
+  if (flow != NULL) {
+    // make sure it is an active UDP flow record
+    if ((flow->active == 0) || (flow->udp != 1))
+      return 0;
+
+    if ((flow->saddr != 0) && (iph->saddr != flow->saddr))
+      return 0;
+
+    if ((flow->daddr != 0) && (iph->daddr != flow->daddr))
+      return 0;
+
+    if ((flow->sport != 0) && (udph->source != flow->sport))
+      return 0;
+
+    if ((flow->dport != 0) && (udph->dest != flow->dport))
+      return 0;
+
+    return 1;
+  }
+  return 0;
+}
+
+// get TCP header
+static inline struct tcphdr* parse_tcp(void *data, u64 nh_off, void *data_end) {
+  struct tcphdr *tcph = data + nh_off;
+
+  if ((void*)&tcph[1] > data_end)
+    return 0;
+
+  return tcph;
+}
+
+// get UDP header
+static inline struct udphdr* parse_udp(void *data, u64 nh_off, void *data_end) {
+  struct udphdr *udph = data + nh_off;
+
+  if ((void*)&udph[1] > data_end)
+    return 0;
+
+  return udph;
+}
+
+// get payload UDP or TCP
+static inline uint16_t *parse_pld(void *data, u64 nh_off, void *data_end) {
+  uint16_t *pld = data + nh_off;
+
+  if ((void*)&pld[1] > data_end)
+    return 0;
+
+  return pld;
+}
+
+
+// note that context type is passed in as a parameter by BCC
+int xdp_tcp_mod_prog(struct CTXTYPE *ctx) {
+
+    void* data_end = (void*)(long)ctx->data_end;
+    void* data = (void*)(long)ctx->data;
+
+    struct ethhdr *eth = data;
+
+    // always pass packets 
+    int rc = XDP_PASS; 
+    uint16_t h_proto;
+    uint64_t nh_off = 0;
+    struct iphdr *ip_hdr;
+    struct tcphdr *tcp_hdr;
+    struct udphdr *udp_hdr;
+    uint16_t *pld;
+
+    nh_off = sizeof(*eth);
+
+    if (data + nh_off  > data_end)
+        return rc;
+
+    h_proto = eth->h_proto;
+
+    if (h_proto == htons(ETH_P_8021Q) || h_proto == htons(ETH_P_8021AD)) {
+        struct vlan_hdr *vhdr;
+
+        vhdr = data + nh_off;
+        nh_off += sizeof(struct vlan_hdr);
+        if (data + nh_off > data_end)
+            return rc;
+    }
+    if (h_proto == htons(ETH_P_8021Q) || h_proto == htons(ETH_P_8021AD)) {
+        struct vlan_hdr *vhdr;
+
+        vhdr = data + nh_off;
+        nh_off += sizeof(struct vlan_hdr);
+        if (data + nh_off > data_end)
+            return rc;
+    }
+
+    // add IPv6 code as needed
+    if (h_proto == htons(ETH_P_IP)) {
+      ip_hdr = parse_ipv4(data, nh_off, data_end);
+    } else
+        ip_hdr = 0;
+
+    if (ip_hdr != 0) {
+      //int idx = 0;
+      //struct flowrec *res;
+      //res=flowspecs.lookup(&idx);
+      //if (res != NULL) {
+      //bpf_trace_printk("%d %d\n", res->saddr, res->daddr);
+      //bpf_trace_printk("%d %d\n", res->sport, res->dport);
+      //bpf_trace_printk("%d %d\n", res->udp, res->active);
+      //}
+      if (ip_hdr->protocol == IPPROTO_TCP) {
+	nh_off += ip_hdr->ihl << 2;
+	tcp_hdr = parse_tcp(data, nh_off, data_end);
+	if (tcp_hdr != 0) {
+	  nh_off += tcp_hdr->doff << 2;
+	  pld = parse_pld(data, nh_off, data_end);
+	  // note that syn packets may not contain any payload
+	  if (pld != 0) {
+	    int idx = 0, match = 0;
+	    struct flowrec *rec;
+	    rec = flowspecs.lookup(&idx);
+	    if (rec != NULL) {
+	      if (rec->active == 0) {
+		// match all TCP packets - no flowspecs in table
+		match = 1;
+	      } else 
+		match = testTcpFlow(ip_hdr, tcp_hdr, rec);
+	    }
+	    if (match != 0)
+	      goto matchtcp;
+	    
+	    idx = 1;
+	    rec = flowspecs.lookup(&idx);
+	    if (rec != NULL)
+	      match = testTcpFlow(ip_hdr, tcp_hdr, rec);
+	    if (match != 0)
+	      goto matchtcp;
+	    
+	    idx = 2;
+	    rec = flowspecs.lookup(&idx);
+	    if (rec != NULL)
+	      match = testTcpFlow(ip_hdr, tcp_hdr, rec);
+	    if (match != 0)
+	      goto matchtcp;
+	    
+	    idx = 3;
+	    rec = flowspecs.lookup(&idx);
+	    if (rec != NULL)
+	      match = testTcpFlow(ip_hdr, tcp_hdr, rec);
+	    if (match != 0)
+	      goto matchtcp;
+	    
+	    idx = 4;
+	    rec = flowspecs.lookup(&idx);
+	    if (rec != NULL)
+	      match = testTcpFlow(ip_hdr, tcp_hdr, rec);
+
+	  matchtcp:
+	    if (match > 0) {
+	      bpf_trace_printk("match TCP\n");
+	      //  int ret = swapu16(pld, data_end, 2, 4);
+	      //bpf_trace_printk("tcp_header length %d ret=%d\n", tcp_hdr->doff, ret);
+	    }
+	  } 
+	}
+      }
+
+      if (ip_hdr->protocol == IPPROTO_UDP) {
+	nh_off += ip_hdr->ihl << 2;
+	udp_hdr = parse_udp(data, nh_off, data_end);
+	if (udp_hdr != 0) {
+	  nh_off += 16;
+	  pld = parse_pld(data, nh_off, data_end);
+	  if (pld != 0) {
+	    int idx = 0, match = 0;
+	    struct flowrec *rec;
+	    rec = flowspecs.lookup(&idx);
+	    if (rec != NULL)
+	      match = testUdpFlow(ip_hdr, udp_hdr, rec);
+	    if (match != 0)
+	      goto matchudp;
+	    
+	    idx = 1;
+	    rec = flowspecs.lookup(&idx);
+	    if (rec != NULL)
+	      match = testUdpFlow(ip_hdr, udp_hdr, rec);
+	    if (match != 0)
+	      goto matchudp;
+	    
+	    idx = 2;
+	    rec = flowspecs.lookup(&idx);
+	    if (rec != NULL)
+	      match = testUdpFlow(ip_hdr, udp_hdr, rec);
+	    if (match != 0)
+	      goto matchudp;
+	    
+	    idx = 3;
+	    rec = flowspecs.lookup(&idx);
+	    if (rec != NULL)
+	      match = testUdpFlow(ip_hdr, udp_hdr, rec);
+	    if (match != 0)
+	      goto matchudp;
+	    
+	    idx = 4;
+	    rec = flowspecs.lookup(&idx);
+	    if (rec != NULL)
+	      match = testUdpFlow(ip_hdr, udp_hdr, rec);
+
+	  matchudp:
+	    if (match > 0) {
+	      bpf_trace_printk("match UDP\n");
+	      //  int ret = swapu16(pld, data_end, 2, 4);
+	    }
+	  }
+	}
+      }
+    }
+
+    return rc;
+}
---
+++ bpf/bcc/xdp_flow_modify.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+# modify TCP payloads to keep checksum
+# probability distribution ? 
+# using either XDP or TC BPF hooks
+
+from bcc import BPF
+import pyroute2
+import time
+import sys
+import argparse
+import collections
+import socket
+import struct
+import warnings
+import atexit
+
+def hostnameToLong(ip):
+    """Return 32-bit representation of IP address or hostname in network byte order"""
+    if ip is None:
+        return 0
+    packetIP = socket.inet_pton(socket.AF_INET, socket.gethostbyname(ip))
+    # always produces a tuple, so take 0th index
+    return socket.htonl(struct.unpack("!L", packetIP)[0])
+
+def portToShort(port):
+    """Return 16-bit representation of port in network byte order"""
+    if port is None:
+        return 0
+    return socket.htons(port)
+
+def protoUdp(proto):
+    """Return 1 if protocol string us UDP, 0 otherwise"""
+    if proto is None:
+        return 0
+    if proto == 'udp':
+        return 1
+    return 0
+
+def unloadBPF(mode, idx):
+    print "Unloading BPF"
+    if mode == BPF.XDP:
+        b.remove_xdp(device)
+    else:
+        ip.tc("del", "clsact", idx)
+        ipdb.release()
+
+flags = 0
+parser = argparse.ArgumentParser(epilog="""
+Each flow can be specified as src=a.b.c.d,dst=m.n.o.p,sport=X,dport=Y\n
+Any of the flow parameters can be omitted and specified in any order.
+No spaces are allowed. Up to 5 flowspecs are allowed.
+""")
+
+parser.add_argument("interface", help="interface to bind program to")
+parser.add_argument("--flow", action="append", help="flow specifier")
+parser.add_argument("-t", "--tc", action="store_true", help="Use TC ingress hook instead of XDP")
+
+args = parser.parse_args()
+
+flowparser = argparse.ArgumentParser()
+flowparser.add_argument("--src", help="TCP flow source IP address")
+flowparser.add_argument("--dst", help="TCP flow destination IP address")
+flowparser.add_argument("--sport", help="TCP flow source port", type=int)
+flowparser.add_argument("--dport", help="TCP flow destination port", type=int)
+flowparser.add_argument("--proto", help="Protocol TCP or UDP, defaults to TCP", choices=['tcp', 'udp'], default='tcp')
+
+device = args.interface
+
+if args.tc:
+    flags |= 2 << 0
+    mode = BPF.SCHED_CLS
+    ctxtype = "__sk_buff"
+else:
+    mode = BPF.XDP
+    ctxtype = "xdp_md"
+
+# list of flowspecs
+flowlist = []
+
+flownum = 0
+
+if args.flow:
+    flownum = len(args.flow)
+    if flownum > 5:
+        print("Too manu flowspecs ({}), exiting. Only 5 allowed".format(flownum))
+        sys.exit(1)
+else:
+    warnings.warn("WARNING: All TCP flows entering interface {} will be modified".format(device))
+
+print ("There are {} flowspecs\n".format(flownum))
+print("Binding to {} using method {}".format(device, mode))
+    
+# load from file
+b = BPF(src_file = "tcp_modify.c", cflags=["-w", "-DCTXTYPE=%s" % ctxtype, "-DFLOWNUM=%d" % flownum], debug = 0)
+
+fn = b.load_func("xdp_tcp_mod_prog", mode)
+
+# attach via TC or XDP
+idx = 0
+if mode == BPF.XDP:
+    b.attach_xdp(device, fn, flags)
+else:
+    ip = pyroute2.IPRoute()
+    ipdb = pyroute2.IPDB(nl=ip)
+    idx = ipdb.interfaces[device].index
+    ip.tc("add", "clsact", idx)
+    ip.tc("add-filter", "bpf", idx, ":1", fd=fn.fd, name=fn.name,
+          parent="ffff:fff2", classid=1, direct_action=True)
+
+# be sure to cleanup on exit
+atexit.register(unloadBPF, mode, idx)
+
+# fill out flowspecs
+flowspecs = b["flowspecs"]
+
+# fill in the flow table
+flowIdx = 0
+if args.flow:
+    for flow in args.flow:
+        flowarg = "--" + flow.replace(",", " --").replace("=", " ")
+        # parse flow specifier
+        flowargs = flowparser.parse_args(flowarg.split())
+        flowspecs[flowspecs.Key(flowIdx)] = flowspecs.Leaf(saddr = hostnameToLong(flowargs.src),
+                                                           daddr = hostnameToLong(flowargs.dst),
+                                                           sport = portToShort(flowargs.sport),
+                                                           dport = portToShort(flowargs.dport),
+                                                           udp = protoUdp(flowargs.proto),
+                                                           active = 1)
+        flowIdx += 1
+
+while 1:
+    try:
+        (saddr, daddr) = b.trace_print()
+        print("Entry has {} {}\n".format(saddr, daddr))
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print("Removing filter from device")
+        break;
+
+
